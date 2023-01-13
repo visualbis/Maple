c7e1b24f1c0400c78401a28d4940b3fc16c12b9b
praveenn77 <npu@live.in>
2023-01-13 18:04:31 +0530

    feat config moved separately

---
+++ config.js
@@ -0,0 +1,57 @@
+
+window.config = [{
+    value: 'ibcslibrary',
+    label: 'Inforiver Matrix',
+    params: [{
+        name: 'buildType',
+        type: 'dropdown',
+        default: 'enterprise',
+        options: [{
+            value: 'enterprise',
+            label: 'Enterprise'
+        }, {
+            value: 'premium',
+            label: 'Premium'
+        }, {
+            value: 'standard',
+            label: 'Standard'
+        }, {
+            value: 'webbuild',
+            label: 'Webbuild'
+        }]
+    },
+    {
+        name: 'mockLicence',
+        label: 'Enable Mock Licence',
+        type: 'switch',
+    },
+    {
+        name: 'serviceUrl',
+        label: 'Service URL(Optional)',
+        type: 'text',
+        optional: true,
+    }]
+},
+{
+    value: 'ValqPowerBI',
+    label: 'ValQ PowerBI',
+    params: []
+},
+{
+    value: 'ibcscharts',
+    label: 'Inforiver Charts',
+    params: [{
+        name: 'buildType',
+        type: 'dropdown',
+        default: 'charts',
+        options: [{
+            value: 'charts',
+            label: 'Charts'
+        }]
+    },
+    {
+        name: 'mockLicence',
+        label: 'Enable Mock Licence',
+        type: 'switch',
+    }]
+}];
